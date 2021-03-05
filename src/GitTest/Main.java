78dbab793b328a83bb8fe23d7a78e8ee93589ea8
JanMindermann <Jan.Mindermann@t-online.de>
2021-03-05 21:28:05 +0100

    Die zweite Änderung an meinem Repository

--- src/GitTest/Main.java
+++ src/GitTest/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("JHkljahsdflkjhasdlfkjhasdlkfjhalsdkfhj");
+
+        int abc=0;
+        for(int i=0; i<200;i++){
+            abc=+i;
+        }
+        System.out.println(abc);
     }
+
+
+
+
 }
